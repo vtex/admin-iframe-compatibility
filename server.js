c25b26cfd568a8d2be3e2e66ca389f7d8cc97187
ohmyguigs <ohmyguigs@gmail.com>
2018-07-13 15:45:55 -0300

    Adds little server to develop locally

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+const app = express()
+
+app.use(express.static('./', {
+  maxAge: '10s',
+  etag: false
+}))
+
+app.listen(4200, () => {
+  console.log('Serving static js in localhost:4200/index.js')
+})
