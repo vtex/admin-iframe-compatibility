a83efa3be812353b37af27bac68245837c7e9c0c
ohmyguigs <ohmyguigs@gmail.com>
2018-07-16 14:54:19 -0300

    Fix 3card grid in RelatorioIndexacao.aspx

--- index.js
+++ index.js
@@ -91,6 +91,10 @@ $(function ($) {
         $(".AspNet-Menu").parent().parent().hide()
         if (!window.location.href.includes('Site/RelatorioIndexacao.aspx')) {
             $("#content h2").hide()
+        } else { // in RelatorioIndexacao.aspx page, header is a div with an h3
+            $('.page-header').hide()
+            // ajusta largura para ficar grid com 3 cards ao inves de 2 com menu lateral aberto
+            $('.container')[0].style.width = "100%"
         }
         $("#areaUsuario").hide()
          // also if inside iframe, dispatch event to myvtex sending content height
