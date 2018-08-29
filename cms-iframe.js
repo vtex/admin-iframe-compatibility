9b341c4f851238d3c69b18eb3b62c7f069a709d5
ohmyguigs <ohmyguigs@gmail.com>
2018-08-29 15:48:14 -0300

    adds topbar instancing code

--- cms-iframe.js
+++ cms-iframe.js
@@ -1,1 +1,5 @@
-console.debug(`%c [ADMIN CMS JS] \n Running custom js `, 'background: #002833; color: #258bd2');
+console.debug(`%c [ADMIN CMS JS] - Running custom js `, 'background: #002833; color: #258bd2');
+
+$(window).on('topbarLoaded.vtex', function () {
+  vtex.topbar.topbar = new vtex.topbar.Topbar();
+});
