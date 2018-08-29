add94fdd6c84db9e38b2e12b81efd98d4b1a0ff3
ohmyguigs <ohmyguigs@gmail.com>
2018-08-29 16:50:02 -0300

    Adds vtexLoader

--- cms-iframe.js
+++ cms-iframe.js
@@ -4,6 +4,13 @@ console.debug(`%c [ADMIN CMS JS] - Running custom js `, 'background: #002833; co
 *  THIS BLOCK HAS SOME HELPER FUNCTIONS  *
 ******************************************/
 
+var addVtexLoaderScript = function () {
+  var vtexLoaderScript = document.createElement('script')
+  vtexLoaderScript.async = false
+  vtexLoaderScript.src = "//io.vtex.com.br/io-vtex-loader/2.3.1/io-vtex-loader.min.js"
+  document.body.appendChild(vtexLoaderScript)
+}
+
 var handleIframePostMessage = (e) => {
   console.debug(`%c [ADMIN CMS JS] - message received: \n data: ${e.data} `, 'background: #002833; color: #258bd2')
 }
@@ -24,6 +31,7 @@ if (window.self !== window.top) {
 if (window.self === window.top) {
   console.debug(`%c [ADMIN CMS JS] - NOT running inside iframe`, 'background: #002833; color: #258bd2')
   // legacy topbar load
+  addVtexLoaderScript();
   $(window).on('topbarLoaded.vtex', function () {
     vtex.topbar.topbar = new vtex.topbar.Topbar();
   });
