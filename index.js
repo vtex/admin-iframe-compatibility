eb11470bd8f4bc3fe9eece43ba6f5543edd68223
ohmyguigs <ohmyguigs@gmail.com>
2019-10-09 17:48:07 -0300

    Checks if final date is invalid before ovewriting it

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ var handleDatesFormat = (lang) => {
         // translate element text based on lang using iso date attribute
         const isoDate = $(e).attr('data-vtex-date-utc')
         const finalDate = new Date(isoDate).toLocaleDateString(lang, { dateStyle: "short" })
-        $(e).text(finalDate)
+        if (finalDate && finalDate !== 'Invalid Date') {
+            $(e).text(finalDate)
+        }
     })
     // DATE TIME ELEMENTS
     var dateTimeElements = $(".vtex-locale-datetime");
@@ -48,7 +50,9 @@ var handleDatesFormat = (lang) => {
         // translate element text based on lang using iso date attribute
         const isoDate = $(e).attr('date-vtex-datetime-utc')
         const finalDate = new Date(isoDate).toLocaleDateString(lang, { dateStyle: "short", timeStyle: "medium" })
-        $(e).text(finalDate)
+        if (finalDate && finalDate !== 'Invalid Date') {
+            $(e).text(finalDate)
+        }
     })
 }
 
